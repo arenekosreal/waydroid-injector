694c29e806e30794e10ab0bc55c9e6db733505d3
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2024-10-13 20:35:45 +0800

    Bump to 0.1.1

--- src/waydroid_injector/__init__.py
+++ src/waydroid_injector/__init__.py
@@ -23,7 +23,7 @@ from waydroid_injector.manifest import Manifest
 from waydroid_injector.type_defines import EntrypointFunctionType
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 def _type_accepted(i: Any, *ts: type | None) -> bool:  # noqa: ANN401
